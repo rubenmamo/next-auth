b97cf136f449c16709b619ed3dd5b558635f1e39
Ruben Mamo <ruben@4inline.com>
2024-07-19 13:15:45 +0200

    Added CosmosDb Adapter

---
+++ packages/adapter-cosmosdb/test/test.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+CONTAINER_NAME=authjs-azure-cosmosdb-test
+
+# Start db
+docker run \
+    --publish 8081:8081 \
+    --publish 10250-10255:10250-10255 \
+    --name ${CONTAINER_NAME}  \
+    --detach \
+    mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator:latest   
+
+echo "Waiting 10s for db to start..."
+sleep 10
+
+
+TESTMODE=NOPK
+if vitest run -c ../utils/vitest.config.ts; then
+  echo "Executed test with no partition key definition"
+else
+  docker stop ${CONTAINER_NAME} && exit 1
+fi
+
+TESTMODE=ID
+
+if vitest run -c ../utils/vitest.config.ts; then
+  echo "Executed test with same as id partition key strategy"
+else
+  docker stop ${CONTAINER_NAME} && exit 1
+fi
+
+TESTMODE=DT
+
+if vitest run -c ../utils/vitest.config.ts; then
+  echo "Executed test with same as id dataType strategy"
+else
+  docker stop ${CONTAINER_NAME} && exit 1
+fi
+
+TESTMODE=HC
+
+# Always stop container, but exit with 1 when tests are failing
+if vitest run -c ../utils/vitest.config.ts; then
+  echo "Executed test with hardcoded partition key strategy"
+  docker stop ${CONTAINER_NAME}
+else
+  docker stop ${CONTAINER_NAME} && exit 1
+fi
